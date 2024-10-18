003a398a56570791e3170861831cbeb050989914
THOMAS  SECONDE  DEVIS <kouamedjakiss766@gmail.com>
2024-10-18 23:19:17 +0000

    Update plugins-activate.js

--- script/plugins-activate.js
+++ script/plugins-activate.js
@@ -1,8 +1,49 @@
-<script>
-    $(document).ready(function() {
-        $(".menu-toggle").click(function() {
-            $("#sidebar-wrapper").toggleClass("active");
-            $(this).toggleClass("active");
-        });
-    });
-</script>
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Ferme le menu responsive quand un lien de déclenchement est cliqué
+  $('.js-scroll-trigger').click(function() {
+    $("#sidebar-wrapper").removeClass("active");
+    $(".menu-toggle").removeClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Active le menu toggle
+  $(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+    $(".menu-toggle").toggleClass("active");
+    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+  });
+
+  // Défilement fluide en utilisant jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Ferme le menu responsive lorsque la fenêtre est redimensionnée au-dessus de 992px
+  $(window).resize(function() {
+    if ($(window).width() > 992) {
+      $("#sidebar-wrapper").removeClass("active");
+      $(".menu-toggle").removeClass("active");
+      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+    }
+  });
+
+  // Ajoute une classe active aux éléments de navigation lors du défilement
+  $('body').scrollspy({
+    target: '#sidebar-wrapper',
+    offset: 100
+  });
+
+})(jQuery); // Fin de l'utilisation stricte
+
